0f60054257bca8a9ca4e0d463baac774aa3846ee
AviNanavare <avi.nanaware@gmail.com>
2021-02-11 12:55:39 +0530

    JDBC CRUD using Servelets

---
+++ Servlet-JDBC/src/database/InsertData.java
@@ -0,0 +1,44 @@
+package database;
+
+import java.io.*;
+import java.sql.*;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class InsertData extends HttpServlet { 
+
+	protected void doPost(HttpServletRequest request,  
+    		HttpServletResponse response)
+    {      
+		int fin = 0;
+    			try {
+    				String url = "jdbc:mysql://localhost:3306/testdatabase";
+    				String user = "root";
+    				String pass = "root";
+    				String driver = "com.mysql.cj.jdbc.Driver";
+    				
+    		    	Class.forName(driver);
+    		    	Connection con = DriverManager.getConnection(url,user,pass);
+        			response.setContentType("text/html");
+        			PrintWriter out=response.getWriter();		
+        			
+                	PreparedStatement st = con.prepareStatement
+    	            		("insert into employee values(?, ?, ?)"); 
+                	
+    	            st.setInt(1,Integer.parseInt(request.getParameter("id"))); 
+    	            st.setString(2,request.getParameter("name")); 
+    	            st.setInt(3,Integer.parseInt(request.getParameter("age")));
+    	            fin = st.executeUpdate();
+    	            out.println("<html><body><b>"
+    	            		+"Successful"+ "</b><br>"
+    	            		+fin+"Row's Added . . . "+ "</body></html>");
+    	            
+    	            st.close();
+    	            con.close();
+    	            
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+	            System.out.println(fin+"Row's Added . . . ");
+    } 
+} 
